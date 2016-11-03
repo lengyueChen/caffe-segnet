c57655030b45f43d9b9b34c1ce553bbe4fdb0e78
Xue Xu <xuexu@wirelessprv-10-192-101-49.near.illinois.edu>
2016-11-03 17:01:40 -0500

    fwd

--- src/caffe/layers/intersection_over_union_layer.cpp
+++ src/caffe/layers/intersection_over_union_layer.cpp
@@ -32,15 +32,8 @@ void IntersectionOverUnionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& 
 	const Dtype* bottom_label= bottom[1]->mutable_cpu_data();
 	Dtype* top_data = top[0]->mutable_cpu_data();
 	const int num = bottom[0]->num();
-	const int classes = bottom[0]->channels();
 	const int height = bottom[0]->height();
 	const int width = bottom[0]->width();
-	/*
-	std::cout<< "Num: "<< bottom[0]->num() << std::endl;
-	std::cout<< "Classes:"<< bottom[0]->channels() << std::endl;
-	std::cout<< "Height:"<< bottom[0]->height() << std::endl;
-	std::cout<< "Width:"<< bottom[0]->width() << std::endl;
-	*/
 
 	float IUscore = 0.0;
 	// C_i  number of correctly classified pixels in class i. 
@@ -51,7 +44,7 @@ void IntersectionOverUnionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& 
 	int P_i = 0;
 
 
-	for (int class_idx = 0; class_idx < classes; class_idx++){
+	for (int class_idx = 0; class_idx < num_class; class_idx++){
 		C_i=0;
 		G_i=0;
 		P_i=0;
@@ -71,58 +64,6 @@ void IntersectionOverUnionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& 
 			}       
 		}
 
-
-
-		// for(int n = 0; n < num; n++){
-		// 	for(int h = 0; h < height; h++){
-		// 		for(int w = 0; w < width; w++){
-		// 			const int pred_idx = ((n * classes + class_idx) * height + h) * width + w;
-		// 			const int label_idx = (n * height + h) * width + w;
-		// 			//std::cout << "pred_idx: "<< pred_idx << std::endl;
-		// 			//std::cout << "bottom_data: " << bottom_data[pred_idx]<< std::endl;
-		// 			if(bottom_data[pred_idx]== 1 && bottom_label[label_idx] == class_idx+1) 
-		// 				C_i++;
-		// 		}
-		// 	}
-		// }
-		// //calculate G_i. 
-		// // prediction in all class,  ground truth in class_idx
-		// for(int n = 0; n < num;n++){
-		// 	for(int i = 0 ; i < classes; i++){
-		// 		for(int h = 0; h < height; h++){
-		// 			for(int w = 0; w < width; w++){
-		// 				const int pred_idx = ((n * classes + i) * height + h) * width + w;
-		// 				const int label_idx = (n * height + h) * width + w;
-		// 				if (bottom_data[pred_idx]== 1 && bottom_label[label_idx]== class_idx+1)
-		// 					G_i++;
-		// 			}
-		// 		}
-		// 		//std::cout<<std::endl;
-		// 	}
-		// }
-		// //calculate P_i
-		// //predicting class_idx, ground truth in all class
-		// for(int n = 0; n < num;n++){
-		// 	for(int i = 0; i < classes;i++){
-		// 		for(int h = 0; h < height; h++){
-		// 			for(int w = 0; w < width; w++){
-		// 				const int pred_idx = ((n * classes + class_idx) * height + h) * width + w;
-		// 				const int label_idx = (n * height + h) * width + w;
-		// 					//std::cout << "pred_idx: "<< pred_idx << std::endl;
-		// 					//std::cout << "bottom_data: " << bottom_data[pred_idx]<< std::endl;
-		// 					//std::cout << "bottom_label: "<< bottom_label[label_idx] << std::endl;
-		// 				if(bottom_data[pred_idx]==1 && bottom_label[label_idx] == i+1){
-		// 					P_i++;
-		// 					//std::cout << "TRUE" << std::endl;
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
-		//calculate IU for each class
-		//std::cout << "C_i:"<< C_i <<std::endl;
-		//std::cout << "G_i: "<< G_i <<std::endl;
-		//std::cout << "P_i: "<< P_i <<"\n"<<std::endl;
 		std::cout<<"fwd class :"<< class_idx <<std::endl;
 		std::cout<<"C_i: "<<C_i<<std::endl;
 		std::cout<<"G_i: "<<G_i<<std::endl;
