d18b66de5d78a2ceec24d6429784d10844b334e6
Xue Xu <xuexu@wirelessprv-10-192-101-49.near.illinois.edu>
2016-11-07 17:36:15 -0600

    add iou layer set up

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -660,12 +660,18 @@ template <typename Dtype>
 class IntersectionOverUnionLayer : public LossLayer<Dtype> {
  public:
   explicit IntersectionOverUnionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
+      : LossLayer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
 
   virtual inline const char* type() const { return "IntersectionOverUnion"; }
   virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return -1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  virtual inline int MaxTopBlobs() const { return 2; }
 
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
