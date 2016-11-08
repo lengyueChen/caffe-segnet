86b65fed9a4de07b64e263ae4878d20590fdd81a
Xue Xu <xuexu@wirelessprv-10-192-101-49.near.illinois.edu>
2016-11-07 18:27:09 -0600

    iou layer setup

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -668,7 +668,7 @@ class IntersectionOverUnionLayer : public LossLayer<Dtype> {
       const vector<Blob<Dtype>*>& top);
 
   virtual inline const char* type() const { return "IntersectionOverUnion"; }
-  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumBottomBlobs() const { return -1; }
   virtual inline int ExactNumTopBlobs() const { return -1; }
   virtual inline int MinTopBlobs() const { return 1; }
   virtual inline int MaxTopBlobs() const { return 2; }
